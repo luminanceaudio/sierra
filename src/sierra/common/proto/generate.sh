72928676e13cae9dc2c25c36069ad42d9d80e324
Lumi <lumi4x@proton.me>
2024-05-14 18:27:11 +0300

    download protoc on go generate

--- src/sierra/common/proto/generate.sh
+++ src/sierra/common/proto/generate.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
+if [ ! -f "bin/bin/protoc" ]; then
+    echo "src/sierra/common/proto: downloading protoc"
+  
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+    ARCH=$(uname -m)
+    
+    if [[ "$OS" == "darwin" ]]; then
+        OS="osx"
+    fi
+    
+    if [[ "$ARCH" == "arm64" ]]; then
+        ARCH="aarch_64"
+    fi
+    
+    # Download protobuf release
+    curl -LOs https://github.com/protocolbuffers/protobuf/releases/download/v26.1/protoc-26.1-${OS}-${ARCH}.zip
+    
+    # Unzip the downloaded file
+    mkdir -p bin/protoc
+    unzip protoc-26.1-${OS}-${ARCH}.zip -d bin/ >/dev/null
+    
+    # Provide execution permissions
+    chmod +x bin/bin/protoc
+    
+    # Clean up
+    rm protoc-26.1-${OS}-${ARCH}.zip
+fi
+
 if [ ! -d "node_modules" ]; then
-    echo "src/common/proto: node_modules is missing, running 'npm ci'"
+    echo "src/sierra/common/proto: node_modules is missing, running 'npm ci'"
     npm ci
-    echo "src/common/proto: done running 'npm ci'"
-else
-    echo "src/common/proto: node_modules exists, skipping 'npm ci'"
+    echo "src/sierra/common/proto: done running 'npm ci'"
 fi
--- src/sierra/services/sierra/internal/proto/generate.sh
+++ src/sierra/services/sierra/internal/proto/generate.sh
@@ -1,5 +1,9 @@
 mkdir -p ../../../../packages/varsoapi/src/app
-echo src/services/app/internal/proto: Installing protoc-gen-go
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+if ! command -v protoc-gen-go &> /dev/null; then
+  echo "src/sierra/services/sierra/internal: protoc-gen-go not found, installing..."
+  echo src/services/app/internal/proto: Installing protoc-gen-go
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+fi
+
 mkdir -p ../../../../../electron/src/proto/app
-protoc -I=./ --plugin=$(go env GOPATH)/bin/protoc-gen-go --plugin=../../../../common/proto/node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=esModuleInterop=true --go_out=../../ --ts_proto_out=../../../../../electron/src/proto/app appbase.proto apprequests.proto
+../../../../common/proto/bin/bin/protoc -I=./ --proto_path=../../../../common/proto/bin/include --plugin=$(go env GOPATH)/bin/protoc-gen-go --plugin=../../../../common/proto/node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=esModuleInterop=true --go_out=../../ --ts_proto_out=../../../../../electron/src/proto/app appbase.proto apprequests.proto
